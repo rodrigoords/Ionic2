f492c1e0a9c1cbaffc89d76e9f3b423da8dada09
Rodrigo Sene <rodrigoords@hotmail.com>
2017-04-15 18:16:00 -0300

    Alteração e configuração de pacotes

--- google-maps/src/app/app.module.ts
+++ google-maps/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { Device } from '@ionic-native/device';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { GoogleMaps } from "@ionic-native/google-maps";
+import { Geolocation } from '@ionic-native/geolocation';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -26,6 +28,8 @@ import { HomePage } from '../pages/home/home';
     StatusBar,
     SplashScreen,
     GoogleMaps,
+    Device,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- google-maps/src/pages/home/home.ts
+++ google-maps/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 import {GoogleMaps, GoogleMap, LatLng, GoogleMapsEvent} from "@ionic-native/google-maps";
 
 @Component({
@@ -11,40 +12,42 @@ export class HomePage {
   location: any;
   map: GoogleMap;
 
-  constructor(public navCtrl: NavController, private googleMaps: GoogleMaps, public platform: Platform) {
-    this.platform.ready().then(() => {
+  constructor(public navCtrl: NavController, public platform: Platform, public googleMaps: GoogleMaps, public geolocation: Geolocation) {
+    platform.ready().then(() => {
       this.loadMap();
     });
   }
+
   loadMap() {
-    this.location = new LatLng(-34.9290,138.6010);
-    this.map = new GoogleMap('map', {
-      'backgroundColor': 'white',
-      'controls': {
-        'compass': true,
-        'myLocationButton': true,
-        'indoorPicker': true,
-        'zoom': true,
-        'center': location
-      },
-      'gestures': {
-        'scroll': true,
-        'tilt': true,
-        'rotate': true,
-        'zoom': true
-      },
-      'camera': {
-        'latLng': location,
-        'tilt': 30,
-        'zoom': 15,
-        'bearing': 50
-      }
-    });
+    let watch = this.geolocation.watchPosition();
+      watch.subscribe((data) => {
+        let location = new LatLng(data.coords.latitude, data.coords.longitude);
 
-    this.map.setCenter(this.location);
+        this.map = new GoogleMap('map', {
+          'backgroundColor': 'white',
+          'controls': {
+            'compass': true,
+            'myLocationButton': false,
+            'indoorPicker': true,
+            'zoom': false
+          },
+          'gestures': {
+            'scroll': true,
+            'tilt': true,
+            'rotate': true,
+            'zoom': true
+          },
+          'camera': {
+            'latLng': location,
+            'tilt': 30,
+            'zoom': 15,
+            'bearing': 50
+          }
+        });
 
-    this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
-    console.log('Map is ready!');
-    });
+        this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+          console.log('Map is ready!');
+        });
+      });
   }
 }
