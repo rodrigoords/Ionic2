9e36fa513b5a97f98a18ebb4fa8c7b9755d5b383
André Qz <andrewqz@gmail.com>
2017-04-15 19:28:32 -0300

    Correção do Mapa e inclusão da versão fullscreen

--- google-maps/src/pages/home/home.ts
+++ google-maps/src/pages/home/home.ts
@@ -1,7 +1,7 @@
+import { Geolocation } from '@ionic-native/geolocation';
 import { Component } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
-import { Geolocation } from '@ionic-native/geolocation';
-import {GoogleMaps, GoogleMap, LatLng, GoogleMapsEvent} from "@ionic-native/google-maps";
+import { GoogleMaps, GoogleMap, GoogleMapsEvent, LatLng } from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-home',
@@ -9,45 +9,126 @@ import {GoogleMaps, GoogleMap, LatLng, GoogleMapsEvent} from "@ionic-native/goog
 })
 export class HomePage {
 
-  location: any;
   map: GoogleMap;
 
-  constructor(public navCtrl: NavController, public platform: Platform, public googleMaps: GoogleMaps, public geolocation: Geolocation) {
+  constructor(public navCtrl: NavController, public platform: Platform,
+    private googleMaps: GoogleMaps, private geolocation: Geolocation) {
     platform.ready().then(() => {
       this.loadMap();
     });
   }
 
   loadMap() {
+
     let watch = this.geolocation.watchPosition();
-      watch.subscribe((data) => {
-        let location = new LatLng(data.coords.latitude, data.coords.longitude);
+    watch.subscribe((data) => {
+      let location = new LatLng(data.coords.latitude, data.coords.longitude);
 
-        this.map = new GoogleMap('map', {
-          'backgroundColor': 'white',
-          'controls': {
-            'compass': true,
-            'myLocationButton': false,
-            'indoorPicker': true,
-            'zoom': false
+      this.map = new GoogleMap('map', {
+        'styles': [
+          { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+          { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+          { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+          {
+            featureType: 'administrative.locality',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#d59563' }]
           },
-          'gestures': {
-            'scroll': true,
-            'tilt': true,
-            'rotate': true,
-            'zoom': true
+          {
+            featureType: 'poi',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#d59563' }]
           },
-          'camera': {
-            'latLng': location,
-            'tilt': 30,
-            'zoom': 15,
-            'bearing': 50
+          {
+            featureType: 'poi.park',
+            elementType: 'geometry',
+            stylers: [{ color: '#263c3f' }]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#6b9a76' }]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{ color: '#38414e' }]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry.stroke',
+            stylers: [{ color: '#212a37' }]
+          },
+          {
+            featureType: 'road',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#9ca5b3' }]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{ color: '#746855' }]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [{ color: '#1f2835' }]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#f3d19c' }]
+          },
+          {
+            featureType: 'transit',
+            elementType: 'geometry',
+            stylers: [{ color: '#2f3948' }]
+          },
+          {
+            featureType: 'transit.station',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#d59563' }]
+          },
+          {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [{ color: '#17263c' }]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#515c6d' }]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [{ color: '#17263c' }]
           }
-        });
-
-        this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
-          console.log('Map is ready!');
-        });
+        ],
+        'backgroundColor': 'darkblue',
+        'controls': {
+          'compass': true,
+          'myLocationButton': true,
+          'indoorPicker': true,
+          'zoom': false
+        },
+        'gestures': {
+          'scroll': true,
+          'tilt': true,
+          'rotate': true,
+          'zoom': true
+        },
+        'camera': {
+          'latLng': location,
+          'tilt': 30,
+          'zoom': 15,
+          'bearing': 50
+        }
       });
+
+      this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+        console.log('Map is ready!');
+      });
+    });
   }
 }
